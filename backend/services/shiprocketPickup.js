77e032928fda621b01f623ab6496feb270e53bad
Prince Dixit <dixitprince895@gmail.com>
2026-01-08 20:01:47 +0530

    Update shiprocketPickup.js

--- backend/services/shiprocketPickup.js
+++ backend/services/shiprocketPickup.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getShiprocketToken } from "./shipRocket.js";
+import { getShiprocketToken } from "./shiprocket.js";
 // ---------------- CREATE PICKUP ----------------
 export const createPickupLocation = async (merchant) => {
   const token = await getShiprocketToken();
